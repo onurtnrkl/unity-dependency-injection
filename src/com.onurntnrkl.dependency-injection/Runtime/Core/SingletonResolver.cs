dc42db478e8c6da90d4a362775eb6efabdf181fb
Onur Tanrıkulu <onurtnrkl@gmail.com>
2023-08-05 14:07:20 +0300

    feat: add singleton resolver

---
+++ src/com.onurntnrkl.dependency-injection/Runtime/Core/SingletonResolver.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using System;
+using System.Reflection;
+
+namespace DependencyInjection.Core
+{
+    internal sealed class SingletonResolver : IResolver
+    {
+        private readonly Type _implementationType;
+        private object _implementation;
+
+        public SingletonResolver(Type implementationType)
+        {
+            _implementationType = implementationType;
+        }
+
+        public object Resolve()
+        {
+            if (_implementation == null)
+            {
+                var constructor = _implementationType.GetConstructors(BindingFlags.Public | BindingFlags.Instance)[0];
+                _implementation = RuntimeHelpers.GetUninitializedObject(_implementationType);
+                constructor.Invoke(_implementation, null);
+            }
+
+            return _implementation;
+        }
+    }
+}
