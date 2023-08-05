ef4a52e427cea0d89f671f58a43bdf7c27cd95bf
Onur Tanrıkulu <onurtnrkl@gmail.com>
2023-08-05 14:18:25 +0300

    test(core): add resolve instance and singleton without parameter test

---
+++ src/com.onurntnrkl.dependency-injection/Tests/Editor/ContainerTests.cs
@@ -0,0 +1,28 @@
+﻿using DependencyInjection.Core;
+using DependencyInjection.EditorTests.Fakes;
+using NUnit.Framework;
+
+namespace DependencyInjection.EditorTests
+{
+    internal partial class ContainerTests
+    {
+
+        [Test]
+        public void Resolve_InstanceOfZeroParameterClassWithRegistrationType_ShouldReturnInstanceOfImplementation()
+        {
+            var container = new Container();
+            container.AddInstance<IZeroParameterClass, ZeroParameterClass>(new ZeroParameterClass());
+            var instance = container.Resolve(typeof(IZeroParameterClass));
+            Assert.IsInstanceOf<IZeroParameterClass>(instance);
+        }
+
+        [Test]
+        public void Resolve_SingletonOfZeroParameterClassWithRegistrationType_ShouldReturnInstanceOfImplementation()
+        {
+            var container = new Container();
+            container.AddSingleton<IZeroParameterClass, ZeroParameterClass>();
+            var instance = container.Resolve(typeof(IZeroParameterClass));
+            Assert.IsInstanceOf<IZeroParameterClass>(instance);
+        }
+    }
+}
