76794b225a8a7cd14c80e840e2ccea3da4741f70
Onur Tanrıkulu <onurtnrkl@gmail.com>
2023-08-05 14:07:05 +0300

    feat: add instance resolver

---
+++ src/com.onurntnrkl.dependency-injection/Runtime/Core/InstanceResolver.cs
@@ -0,0 +1,17 @@
+﻿namespace DependencyInjection.Core
+{
+    internal sealed class InstanceResolver : IResolver
+    {
+        private readonly object _implementation;
+
+        public InstanceResolver(object implementation)
+        {
+            _implementation = implementation;
+        }
+
+        public object Resolve()
+        {
+            return _implementation;
+        }
+    }
+}
