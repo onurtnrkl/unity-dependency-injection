b42d380cc1544fd4cf21f8ca7f1abbbc3a539e5d
Onur Tanrıkulu <onurtnrkl@gmail.com>
2023-08-08 17:18:49 +0300

    feat: add fixed size array pool

---
+++ src/com.onurtnrkl.dependency-injection/Runtime/Pool/FixedSizeArrayPool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Pool;
+
+namespace DependencyInjection.Pool
+{
+    internal class FixedSizeArrayPool<T>
+    {
+        private static readonly Dictionary<int, IObjectPool<T[]>> s_poolsBySizes = new();
+
+        public static T[] Get(int size)
+        {
+            var pool = GetOrCreatePool(size);
+            return pool.Get();
+        }
+
+        public static IDisposable Get(int size, out T[] array)
+        {
+            var pool = GetOrCreatePool(size);
+            return pool.Get(out array);
+        }
+
+        public static void Release(T[] array)
+        {
+            var size = array.Length;
+            var pool = GetOrCreatePool(size);
+            pool.Release(array);
+        }
+
+        private static IObjectPool<T[]> GetOrCreatePool(int size)
+        {
+            if (!s_poolsBySizes.TryGetValue(size, out var pool))
+            {
+                pool = new ObjectPool<T[]>(() => new T[size], null, (T[] array) => Array.Clear(array, 0, size));
+                s_poolsBySizes.Add(size, pool);
+            }
+
+            return pool;
+        }
+    }
+}
