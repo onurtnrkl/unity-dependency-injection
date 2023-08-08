1818ea1a945b2bec48b4ff009a46240588987b6e
Onur Tanrıkulu <onurtnrkl@gmail.com>
2023-08-08 11:49:22 +0300

    feat: add injection info cache

---
+++ src/com.onurtnrkl.dependency-injection/Runtime/Caching/InjectionInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+
+namespace DependencyInjection.Caching
+{
+    internal sealed class InjectionInfo
+    {
+        public Action<object, object[]> Activator { get; }
+        public Type[] ParameterTypes { get; }
+
+        private InjectionInfo(Action<object, object[]> activator, Type[] parameterTypes)
+        {
+            Activator = activator;
+            ParameterTypes = parameterTypes;
+        }
+
+        public static InjectionInfo FromMethodBase(MethodBase methodBase)
+        {
+            var parameterInfos = methodBase.GetParameters();
+            var parameterTypes = Array.ConvertAll(parameterInfos, (parameterInfo) => parameterInfo.ParameterType);
+
+            void Activator(object obj, object[] parameters)
+            {
+                methodBase.Invoke(obj, parameters);
+            }
+
+            return new InjectionInfo(Activator, parameterTypes);
+        }
+    }
+}
---
+++ src/com.onurtnrkl.dependency-injection/Runtime/Caching/InjectionInfoCache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DependencyInjection.Caching
+{
+    internal sealed class InjectionInfoCache
+    {
+        private readonly IDictionary<Type, InjectionInfo> _injectionInfosByImplementationTypes;
+
+        public InjectionInfoCache()
+        {
+            _injectionInfosByImplementationTypes = new Dictionary<Type, InjectionInfo>();
+        }
+
+        public void Add(Type implementationType, InjectionInfo injectionInfo)
+        {
+            _injectionInfosByImplementationTypes.Add(implementationType, injectionInfo);
+        }
+
+        public InjectionInfo Get(Type type)
+        {
+            return _injectionInfosByImplementationTypes[type];
+        }
+    }
+}
