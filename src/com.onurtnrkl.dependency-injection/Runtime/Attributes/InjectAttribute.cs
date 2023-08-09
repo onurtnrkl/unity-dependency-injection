05b9831c537aa91585afee4071360dc0c58dadf3
Onur Tanrıkulu <onurtnrkl@gmail.com>
2023-08-09 10:46:46 +0300

    feat: add inject attribute

---
+++ src/com.onurtnrkl.dependency-injection/Runtime/Attributes/InjectAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine.Scripting;
+
+namespace DependencyInjection.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class InjectAttribute : PreserveAttribute
+    {
+    }
+}
