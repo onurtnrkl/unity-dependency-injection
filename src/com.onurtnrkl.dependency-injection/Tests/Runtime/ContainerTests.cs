84eac928e719eb90ffab55fba76fdaadf4b475f2
Onur Tanrıkulu <onurtnrkl@gmail.com>
2023-08-22 10:15:23 +0300

    test: add prefab resolver tests

---
+++ src/com.onurtnrkl.dependency-injection/Tests/Runtime/ContainerTests.cs
@@ -0,0 +1,23 @@
+﻿using DependencyInjection.Core;
+using DependencyInjection.Tests.Fakes;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace DependencyInjection.Tests
+{
+    internal sealed class ContainerTests
+    {
+        [Test]
+        public void Resolve_ContainerWithPrefab_ShouldBeInjectedWithZeroParameterClass()
+        {
+            var containerBuilder = new ContainerBuilder(Container.Root);
+            var prefab = new GameObject("OneParameterMonoBehaviour", typeof(OneParameterMonoBehaviour));
+            containerBuilder.AddSingleton(typeof(OneParameterMonoBehaviour), prefab);
+            containerBuilder.AddSingleton(typeof(IZeroParameterClass), typeof(ZeroParameterClass));
+            var container = containerBuilder.Build();
+            var component = (OneParameterMonoBehaviour)container.Resolve(typeof(OneParameterMonoBehaviour));
+
+            Assert.IsNotNull(component.GetZeroParameterClass());
+        }
+    }
+}
